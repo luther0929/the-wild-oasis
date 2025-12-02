df47bd53bca5be8c2199d3b34ef6057d64b92afa
Your Name <evangelista.luther1995@gmail.com>
2025-12-02 21:22:30 +1000

    style(app): add styled-components to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const SyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <SyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('check in')}>Check in</Button>
+        <Button onClick={() => alert('check out')}>Check out</Button>
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
+      </SyledApp>
+    </>
+  );
 }
 
 export default App;
